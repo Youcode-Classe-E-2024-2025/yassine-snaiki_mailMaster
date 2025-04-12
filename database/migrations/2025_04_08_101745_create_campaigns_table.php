0656cd904b8cd07db88b9aa04949ec1a11b9ee22
yassine snaiki <yassnaiki@gmail.com>
2025-04-12 12:28:31 +0100

    campaign migration

---
+++ database/migrations/2025_04_08_101745_create_campaigns_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        
+     Schema::create('campaigns', function (Blueprint $table) {
+        $table->id();
+        $table->string('subject');
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->foreignId('newsletter_id')->constrained()->onDelete('cascade');
+        $table->enum('status', ['ongoing' ,'end'])->default('ongoing');
+         
+        $table->timestamps();
+
+    });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaigns');
+    }
+};
